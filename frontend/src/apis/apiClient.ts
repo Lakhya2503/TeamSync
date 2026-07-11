ce5b1b31dd543546a3a051ee967f9ceaafed74bf
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-07-11 23:16:29 +0530

    fix: update base URL references in apiClient and socketStore

--- frontend/src/apis/apiClient.ts
+++ frontend/src/apis/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl : string = import.meta.env.VITE_BACKEND_URL || "http://localhost:5002/api/v1/teamsync"
+const baseUrl : string = import.meta.env.VITE_API_BASE_URL || "http://localhost:5002/api/v1/teamsync"
 
 export const apiClient = axios.create({
     baseURL : baseUrl,
--- frontend/src/app/socketStore.ts
+++ frontend/src/app/socketStore.ts
@@ -1,12 +1,14 @@
 import { io } from "socket.io-client"
 
+const socketUrl: string = import.meta.env.VITE_SOCKET_URL || "http://localhost:5002"
+
 export const useSocket = () => {
 
     const socket = null
 
     const socketConnect = () => {
         const newSocket = io(
-            import.meta.env.BASE_URL,
+            socketUrl,
             {
                 withCredentials : true,
                 transports : ['websocket'],
