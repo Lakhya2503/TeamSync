fb034acf8f74f5391079d709f428a3590a97aa26
Lakhya2503 <25032002shindelaxman@gmail.com>
2026-07-12 00:10:47 +0530

    feat: add Header, Layout, and Sidebar components for dashboard structure

---
+++ frontend/src/Components/Dashboard/layout/Layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <>
+        {/* SIDEBAR */}
+
+
+        {/* HEADER */}
+
+
+        {/* MAIN */}
+        
+    
+    </>
+  )
+}
+
+export default Layout
